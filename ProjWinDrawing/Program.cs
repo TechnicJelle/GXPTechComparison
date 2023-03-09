a1a454b95fe2e04165e5d5adcda14aa5899bd16a
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2023-03-09 14:13:18 +0100

    WinDraw started with spawning window&setup GL(FW)

--- ProjWinDrawing/Program.cs
+++ ProjWinDrawing/Program.cs
@@ -1,12 +1,79 @@
 ﻿using System;
+using ProjWinDrawing.bindings;
 
-namespace ProjWinDrawing
+namespace ProjWinDrawing;
+
+internal static class Program
 {
-	internal class Program
+	// Settings
+	private const int SCR_WIDTH = 800;
+	private const int SCR_HEIGHT = 600;
+
+	private const string WINDOW_TITLE = "ProjSkiaSharp";
+
+	public static int Main(string[] args)
 	{
-		public static void Main(string[] args)
+		// GLFW: Initialize and Configure
+		GLFW.Init();
+
+		// GLFW: Window Creation
+		GLFW.OpenWindowHint(GLFW.FSAA_SAMPLES, 8);
+		GLFW.OpenWindow(SCR_WIDTH, SCR_HEIGHT, 8, 8, 8, 8, 24, 0, GLFW.WINDOWED);
+		GLFW.SetWindowTitle(WINDOW_TITLE);
+
+		GLFW.SetWindowSizeCallback(WindowSizeCallback);
+		GLFW.SwapInterval(false); // VSync
+
+		Console.WriteLine("GLFW window created");
+
+		// Render Loop
+		double lastTime = GLFW.GetTime();
+		double fpsTimer = GLFW.GetTime();
+		while (GLFW.GetWindowParam(GLFW.ACTIVE) == 1)
 		{
-			Console.WriteLine("Hello World!");
+			double deltaTime = GLFW.GetTime() - lastTime;
+			lastTime = GLFW.GetTime();
+			// Input
+			ProcessInput();
+
+			// Change Window Title to show FPS, every second
+			if (GLFW.GetTime() - fpsTimer >= 1.0)
+			{
+				GLFW.SetWindowTitle($"{WINDOW_TITLE} - FPS: " + Math.Round(1.0 / deltaTime));
+				fpsTimer = GLFW.GetTime();
+			}
+
+			// Render
+			// Clear the screen
+			GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+			GL.Clear(GL.COLOR_BUFFER_BIT);
+
+			// Draw our textured square
+
+			// GLFW: Swap Buffers and Poll IO Events (keys pressed/released, mouse moved etc.)
+			GLFW.SwapBuffers();
+			GLFW.PollEvents();
 		}
+
+		GLFW.Terminate();
+		return 0;
+	}
+
+	private static void ProcessInput()
+	{
+		if (GLFW.GetKey(Key.ESCAPE))
+			GLFW.CloseWindow();
+	}
+
+	private static void WindowSizeCallback(int newWidth, int newHeight)
+	{
+		GL.Viewport(0, 0, newWidth, newHeight);
+		// GL.Enable(GL.MULTISAMPLE);
+		// GL.Enable(GL.TEXTURE_2D);
+		// GL.Enable(GL.BLEND);
+		// GL.BlendFunc(GL.SRC_ALPHA, GL.ONE_MINUS_SRC_ALPHA);
+		// GL.Hint(GL.PERSPECTIVE_CORRECTION, GL.FASTEST);
+		// //GL.Enable(GL.POLYGON_SMOOTH);
+		// GL.ClearColor(0.0f, 1.0f, 0.0f, 0.0f);
 	}
 }
---
+++ ProjWinDrawing/bindings/GLFW.cs
@@ -0,0 +1,85 @@
+﻿using System.Runtime.InteropServices;
+using System.Security;
+
+namespace ProjWinDrawing.bindings;
+
+public static class GLFW
+{
+	[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+	[SuppressUnmanagedCodeSecurity]
+	public delegate void KeyCallback(int key, int action);
+
+	[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+	[SuppressUnmanagedCodeSecurity]
+	public delegate void MouseButtonCallback(int button, int action);
+
+	[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+	[SuppressUnmanagedCodeSecurity]
+	public delegate void WindowSizeCallback(int width, int height);
+
+	public const int OPENED			= 0x00020001;
+	public const int WINDOWED		= 0x00010001;
+	public const int FULLSCREEN		= 0x00010002;
+	public const int ACTIVE			= 0x00020001;
+	public const int FSAA_SAMPLES	= 0x0002100E;
+	public const int MOUSE_CURSOR	= 0x00030001;
+
+	// Uses the GLFW.dll from the lib folder
+
+	[DllImport("lib/glfw.dll", EntryPoint = "glfwSetTime")]
+	public static extern void SetTime(double time);
+
+	[DllImport("lib/glfw.dll", EntryPoint = "glfwGetTime")]
+	public static extern double GetTime();
+
+	[DllImport("lib/glfw.dll", EntryPoint = "glfwPollEvents")]
+	public static extern void PollEvents();
+
+	[DllImport("lib/glfw.dll", EntryPoint = "glfwGetWindowParam")]
+	public static extern int GetWindowParam(int param);
+
+	[DllImport("lib/glfw.dll", EntryPoint = "glfwInit")]
+	public static extern void Init();
+
+	[DllImport("lib/glfw.dll", EntryPoint = "glfwOpenWindow")]
+	public static extern void OpenWindow(int width, int height, int r, int g, int b, int a, int depth, int stencil, int mode);
+
+	[DllImport("lib/glfw.dll", EntryPoint = "glfwSetWindowTitle")]
+	public static extern void SetWindowTitle(string title);
+
+	[DllImport("lib/glfw.dll", EntryPoint = "glfwSwapInterval")]
+	public static extern void SwapInterval(bool mode);
+
+	[DllImport("lib/glfw.dll", EntryPoint = "glfwSetWindowSizeCallback")]
+	public static extern void SetWindowSizeCallback(WindowSizeCallback callback);
+
+	[DllImport("lib/glfw.dll", EntryPoint = "glfwCloseWindow")]
+	public static extern void CloseWindow();
+
+	[DllImport("lib/glfw.dll", EntryPoint = "glfwTerminate")]
+	public static extern void Terminate();
+
+	[DllImport("lib/glfw.dll", EntryPoint = "glfwSwapBuffers")]
+	public static extern void SwapBuffers();
+
+	[DllImport("lib/glfw.dll", EntryPoint = "glfwGetMouseButton")]
+	public static extern bool GetKey(int key);
+
+	[DllImport("lib/glfw.dll", EntryPoint = "glfwSetKeyCallback")]
+	public static extern void SetKeyCallback(KeyCallback callback);
+
+	[DllImport("lib/glfw.dll", EntryPoint = "glfwGetKey")]
+	public static extern void OpenWindowHint(int name, int value);
+
+	[DllImport("lib/glfw.dll", EntryPoint = "glfwGetMousePos")]
+	public static extern bool GetMousePos(out int x, out int y);
+
+	[DllImport("lib/glfw.dll", EntryPoint = "glfwSetMousePos")]
+	public static extern void SetMouseButtonCallback(MouseButtonCallback callback);
+
+	[DllImport("lib/glfw.dll", EntryPoint = "glfwGetMouseButton")]
+	public static extern void Enable(int property);
+
+	[DllImport("lib/glfw.dll", EntryPoint = "glfwDisable")]
+	public static extern void Disable(int property);
+}
