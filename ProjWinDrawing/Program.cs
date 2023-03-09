d0542fee6529f719ff960474e47bd8005cbcda78
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2023-03-09 15:22:41 +0100

    (WIP) Load and draw image
    Not functional yet. Quad gets drawn with correct vertex colours, but the texture is not applied.

--- ProjWinDrawing/Program.cs
+++ ProjWinDrawing/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
 using ProjWinDrawing.bindings;
 
 namespace ProjWinDrawing;
@@ -26,6 +28,38 @@ internal static class Program
 
 		Console.WriteLine("GLFW window created");
 
+		float[] vertices =
+		{
+			-0.5f, 0.5f, // left top
+			0.5f, 0.5f, // right top
+			0.5f, -0.5f, // right bottom
+			-0.5f, -0.5f, // left bottom
+		};
+
+		float[] uvs =
+		{
+			0.0f, 0.0f, // left top
+			1.0f, 0.0f, // right top
+			1.0f, 1.0f, // right bottom
+			0.0f, 1.0f, // left bottom
+		};
+
+		// load and create a texture
+		int[] texture = new int[1];
+		GL.GenTextures(1, texture);
+		GL.BindTexture(GL.TEXTURE_2D, texture[0]); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+		// set the texture wrapping parameters
+		GL.TexParameteri(GL.TEXTURE_2D, GL.TEXTURE_WRAP_S, GL.CLAMP_TO_EDGE_EXT);
+		GL.TexParameteri(GL.TEXTURE_2D, GL.TEXTURE_WRAP_T, GL.CLAMP_TO_EDGE_EXT);
+		// set texture filtering parameters
+		GL.TexParameteri(GL.TEXTURE_2D, GL.TEXTURE_MIN_FILTER, GL.LINEAR);
+		GL.TexParameteri(GL.TEXTURE_2D, GL.TEXTURE_MAG_FILTER, GL.LINEAR);
+		// load and generate the texture
+		Bitmap bitmap = new("container.jpg");
+		BitmapData data = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+		GL.TexImage2D(GL.TEXTURE_2D, 0, GL.RGBA, bitmap.Width, bitmap.Height, 0, GL.RGBA, GL.UNSIGNED_BYTE, data.Scan0);
+		bitmap.UnlockBits(data);
+
 		// Render Loop
 		double lastTime = GLFW.GetTime();
 		double fpsTimer = GLFW.GetTime();
@@ -49,6 +83,16 @@ internal static class Program
 			GL.Clear(GL.COLOR_BUFFER_BIT);
 
 			// Draw our textured square
+			// GL.BlendFunc(GL.SRC_ALPHA, GL.ONE_MINUS_SRC_ALPHA);
+			GL.Color4ub(255, 0, 255, 255);
+			GL.BindTexture(GL.TEXTURE_2D, texture[0]);
+			GL.EnableClientState(GL.TEXTURE_COORD_ARRAY);
+			GL.EnableClientState(GL.VERTEX_ARRAY);
+			GL.TexCoordPointer(2, GL.FLOAT, 0, uvs);
+			GL.VertexPointer(2, GL.FLOAT, 0, vertices);
+			GL.DrawArrays(GL.QUADS, 0, 4);
+			GL.DisableClientState(GL.VERTEX_ARRAY);
+			GL.DisableClientState(GL.TEXTURE_COORD_ARRAY);
 
 			// GLFW: Swap Buffers and Poll IO Events (keys pressed/released, mouse moved etc.)
 			GLFW.SwapBuffers();
