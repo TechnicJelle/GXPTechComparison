a214b2b4bb0389bb345b2a4283a4cdaec94628e4
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2024-04-22 03:25:13 +0200

    Create 2 benchmarking projs Sys.Drawing&SkiaSharp

---
+++ BenchSkiaSharp/Program.cs
@@ -0,0 +1,36 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using SkiaSharp;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BenchSkiaSharp;
+
+// Benchmarks must be instance methods, static methods are not supported.
+[SuppressMessage("Performance", "CA1822:Mark members as static")]
+public class ImageLoader
+{
+	[Benchmark]
+	public IntPtr LoadImageAndGetPixels()
+	{
+		SKBitmap bitmap = BitmapFlipped(SKBitmap.Decode("assets/textures/container.jpg"));
+		return bitmap.GetPixels();
+	}
+
+	private static SKBitmap BitmapFlipped(SKBitmap bitmap)
+	{
+		SKBitmap flipped = new(bitmap.Width, bitmap.Height);
+		using SKCanvas canvas = new(flipped);
+		canvas.Scale(1, -1, 0, bitmap.Height / 2.0f);
+		canvas.DrawBitmap(bitmap, 0, 0);
+		return flipped;
+	}
+}
+
+static internal class Program
+{
+	private static void Main(string[] args)
+	{
+		Console.WriteLine(new ImageLoader().LoadImageAndGetPixels()); //make sure it works first
+		BenchmarkRunner.Run<ImageLoader>();
+	}
+}
---
+++ BenchWinDrawing/Program.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace BenchWinDrawing;
+
+public class ImageLoader
+{
+	[Benchmark]
+	public IntPtr LoadImageAndGetPixels()
+	{
+		Bitmap bitmap = new("assets/textures/container.jpg");
+		BitmapData data = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+		bitmap.UnlockBits(data);
+		return data.Scan0;
+	}
+}
+
+static internal class Program
+{
+	private static void Main(string[] args)
+	{
+		Console.WriteLine(new ImageLoader().LoadImageAndGetPixels()); //make sure it works first
+		BenchmarkRunner.Run<ImageLoader>();
+	}
+}
